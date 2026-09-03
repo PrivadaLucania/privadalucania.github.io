eda75760587ac3f29ebdbf3bbbe1d6a0eb2d2535
Privada Lucania <condominiolucania27@gmail.com>
2026-09-03 13:48:55 -0600

    Add files via upload

---
+++ tarjetones/vehiculos.js
@@ -0,0 +1,14 @@
+/*
+ * Catálogo de vehículos de Privada Lucania.
+ * Para agregar un registro, copie el bloque B302, cambie la clave y los datos.
+ * Valores admitidos en acceso: "autorizado" o "suspendido".
+ */
+window.VEHICULOS_LUCANIA = {
+  B302: {
+    acceso: "autorizado",
+    marca: "Chevrolet",
+    modelo: "Spark",
+    anio: "2017",
+    placas: "USX-423-H"
+  }
+};
